88b806e111efa0d4697b91f183ac718e704e8bd3
mdbossss <ferrari020203@gmail.com>
2025-03-02 19:56:07 +0100

    testing commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,17 +4,17 @@ import { Observable, tap } from 'rxjs';
 
 @Controller()
 export class AppController {
-
   private readonly logger = new Logger(AppService.name);
 
-  constructor(private readonly appService: AppService) {
-  }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): Observable<string> {
-    return this.appService.getHello()
-      .pipe(
-        tap(message => this.logger.log('SERVICE RESULT - ' + message)),
-      );
+    return this.appService.getHello().pipe(tap((message) => this.logger.log('SERVICE RESULT - ' + message)));
+  }
+
+  @Get('greeting')
+  getGreeting(): Observable<string> {
+    return this.appService.getGreeting().pipe(tap((message) => this.logger.log('SERVICE RESULT - ' + message)));
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,11 @@ import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class AppService {
-
   getHello(): Observable<string> {
     return of('Hello World!');
+  }
+
+  getGreeting(): Observable<string> {
+    return of('Greetings from the backend!');
   }
 }
