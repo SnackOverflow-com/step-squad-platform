be2d131a9459e5b18146a7a64c21d43c9599afcc
mdbossss <ferrari020203@gmail.com>
2025-03-31 17:55:00 +0200

    /auth/me route

--- src/web/auth/auth.controller.ts
+++ src/web/auth/auth.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guard/local-auth.guard';
 import { UserRegisterRequest } from './dto/user-register-request';
 import { UserLoginRequest } from './dto/user-login-request';
 import { UserLoginResponse } from './dto/user-login-response';
+import { JwtAuthGuard } from './guard/jwt-auth.guard';
+import { UserResponse } from '../user/dto/user-response';
+import { UserService } from '../user/user.service';
 
 @Controller('api/auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
 
   @Post('register')
   async register(@Body() userRegisterRequest: UserRegisterRequest): Promise<UserLoginResponse> {
@@ -18,5 +24,11 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   async login(@Body() userLoginRequest: UserLoginRequest): Promise<UserLoginResponse> {
     return this.authService.login(userLoginRequest);
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  async getCurrentUser(@Request() req: { user: { userId: number } }): Promise<UserResponse> {
+    return this.userService.getUserById(req.user.userId);
   }
 }
