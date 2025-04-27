13b278469c5b484aa6791ff16fdafb2d25c13b81
FilipRaic <raic.filip@gmail.com>
2025-04-27 22:00:14 +0200

    Reset password

--- src/web/auth/auth.controller.ts
+++ src/web/auth/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './guard/local-auth.guard';
 import { UserRegisterRequest } from './dto/user-register-request';
 import { UserLoginRequest } from './dto/user-login-request';
 import { UserLoginResponse } from './dto/user-login-response';
+import { ForgotPasswordRequest } from './dto/forgot-password-request';
+import { ResetPasswordRequest } from './dto/reset-password-request';
+import { LocalAuthGuard } from './guard/local-auth.guard';
 
 @Controller('api/auth')
 export class AuthController {
@@ -18,5 +20,15 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   async login(@Body() userLoginRequest: UserLoginRequest): Promise<UserLoginResponse> {
     return this.authService.login(userLoginRequest);
+  }
+
+  @Post('forgot-password')
+  async forgotPassword(@Body() forgotPasswordRequest: ForgotPasswordRequest): Promise<void> {
+    await this.authService.forgotPassword(forgotPasswordRequest.email);
+  }
+
+  @Post('reset-password')
+  async resetPassword(@Body() resetPasswordRequest: ResetPasswordRequest): Promise<void> {
+    await this.authService.resetPassword(resetPasswordRequest.token, resetPasswordRequest.newPassword);
   }
 }
